f17651a163d7e5c8a1e8c00aaee3d8b3ca1cf24c
yotsugi <harukipdr@gmail.com>
2020-03-30 22:52:06 +0900

    tsconfig build error

--- webpack.config.js
+++ webpack.config.js
@@ -2,4 +2,28 @@ const path = require('path');
 
 module.exports = {
     
+    entry: [path.resolve(__dirname,"./src/index.tsx")],
+    output:{
+        filename: "bundle.js",
+        path: path.resolve(__dirname,"dist")
+    },
+
+    module:{
+        rules:[
+            {
+             test:/|.(js|ts|tsx)?$/,
+             use:"ts-loader",
+             exclude:/node_modules/   
+            },
+        ]
+    },
+
+    resolve:{
+        extensions:[".ts","tsx",".js",".json"]
+    },
+
+    devtool:"source-map",
+    devServer:{
+        contentBase:"./dist"
+    }
 };
